02005999608aae7d6a14cd67ed76ba53013d42de
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-04-20 21:02:34 +0300

    d

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,22 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/');
 
+    cy.contains('h1.text-xs-center')
+      .get('contain.text', 'Sign in');
+
+    cy.get('[type="email"]')
+      .type('test231@gmail.com');
+
+    cy.get('[type="password"]')
+      .type('ffefe234_vfe');
+
+    cy.get('[type="submit"]')
+      .should('contain.text', 'Sign in')
+      .click();
+
+    cy.get('.navbar')
+      .should('contain.text', 'test420');
   });
 });
