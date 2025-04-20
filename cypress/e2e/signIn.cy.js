b997931774b81191809d3610797da5ec872749a4
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-04-20 21:05:59 +0300

    f

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,8 +4,8 @@ describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('/');
 
-    cy.contains('h1.text-xs-center')
-      .get('contain.text', 'Sign in');
+    cy.get('h1.text-xs-center')
+      .should('contain.text', 'Sign in');
 
     cy.get('[type="email"]')
       .type('test231@gmail.com');
